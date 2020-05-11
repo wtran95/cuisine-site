14889168bc51e9c733a06e252d86918b9eb81217
Quan Tran <williamtran2207@gmail.com>
2020-05-11 14:14:16 -0400

    go to top button created

--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,21 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     nums[slideIndex - 1].className += " active";
 }
+
+var topButton = document.getElementById("topBtn");
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunc(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
